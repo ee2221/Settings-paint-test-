281142f5283f0d4bc1dac559644522f2a48fa6da
ee2221 <galgut.prit@gmail.com>
2025-06-24 22:01:37 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Scene from './components/Scene';
+import Toolbar from './components/Toolbar';
+import ActionsToolbar from './components/ActionsToolbar';
+import LayersPanel from './components/LayersPanel';
+import ObjectProperties from './components/ObjectProperties';
+import EditControls from './components/EditControls';
+import CameraPerspectivePanel from './components/CameraPerspectivePanel';
+import LightingPanel from './components/LightingPanel';
+
+function App() {
+  return (
+    <div className="w-full h-screen relative">
+      <Scene />
+      <ActionsToolbar />
+      <Toolbar />
+      <LayersPanel />
+      <ObjectProperties />
+      <EditControls />
+      <CameraPerspectivePanel />
+      <LightingPanel />
+    </div>
+  );
+}
+
+export default App;
